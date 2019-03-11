1ad32756aa5b5c87576c25f4809af55f02661421
elenyou <elenyou333@gmail.com>
2019-03-11 10:33:26 -0400

    private channel

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ import { Grid } from 'semantic-ui-react';
 import './App.css';
 import { connect } from 'react-redux';
 
-const App = ({ currentUser, currentChannel }) => (
+const App = ({ currentUser, currentChannel, isPrivateChannel }) => (
   <Grid columns='equal' className='app' style={{background: '#eee'}}>
     <ColorPanel />
     <SidePanel
@@ -19,6 +19,7 @@ const App = ({ currentUser, currentChannel }) => (
         key = {currentChannel && currentChannel.id}
         currentChannel={currentChannel}
         currentUser = {currentUser}
+        isPrivateChannel = {isPrivateChannel}
       />
     </Grid.Column>
     <Grid.Column width={ 4 }>
@@ -29,7 +30,8 @@ const App = ({ currentUser, currentChannel }) => (
 
 const mapStateToProps = state => ({
   currentUser: state.user.currentUser,
-  currentChannel: state.channel.currentChannel
+  currentChannel: state.channel.currentChannel,
+  isPrivateChannel: state.channel.isPrivateChannel
 })
 
 
--- src/components/Messages/MessagesHeader.js
+++ src/components/Messages/MessagesHeader.js
@@ -3,7 +3,7 @@ import { Segment, Header, Input, Icon } from 'semantic-ui-react';
 
 class MessagesHeader extends Component {
     render() {
-        const { channelName, numUniqueUsers, handleSearchChange, searchLoading } = this.props;
+        const { channelName, numUniqueUsers, handleSearchChange, searchLoading, isPrivateChannel } = this.props;
 
         return (
             <Segment clearing>
@@ -15,7 +15,7 @@ class MessagesHeader extends Component {
                 >
                     <span>
                         { channelName }
-                        <Icon name={'star outline'} color="black" />
+                        {!isPrivateChannel && <Icon name={'star outline'} color="black" />}
                     </span>
                     <Header.Subheader>{numUniqueUsers}</Header.Subheader>
                 </Header>
