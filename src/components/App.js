2f8b30a9fe3c790dbae9a48f3515d559c17b0fdf
elenyou <elenyou333@gmail.com>
2019-03-11 14:56:44 -0400

    metpanel

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ import { Grid } from 'semantic-ui-react';
 import './App.css';
 import { connect } from 'react-redux';
 
-const App = ({ currentUser, currentChannel, isPrivateChannel }) => (
+const App = ({ currentUser, currentChannel, isPrivateChannel, userPosts }) => (
   <Grid columns='equal' className='app' style={{background: '#eee'}}>
     <ColorPanel />
     <SidePanel
@@ -23,7 +23,12 @@ const App = ({ currentUser, currentChannel, isPrivateChannel }) => (
       />
     </Grid.Column>
     <Grid.Column width={ 4 }>
-      <MetaPanel />
+      <MetaPanel
+        key={currentChannel && currentChannel.id}
+        userPosts={userPosts}
+        currentChannel={currentChannel}
+        isPrivateChannel={isPrivateChannel}
+      />
     </Grid.Column>
   </Grid>
 )
@@ -31,7 +36,8 @@ const App = ({ currentUser, currentChannel, isPrivateChannel }) => (
 const mapStateToProps = state => ({
   currentUser: state.user.currentUser,
   currentChannel: state.channel.currentChannel,
-  isPrivateChannel: state.channel.isPrivateChannel
+  isPrivateChannel: state.channel.isPrivateChannel,
+  userPosts: state.channel.userPosts
 })
 
 
